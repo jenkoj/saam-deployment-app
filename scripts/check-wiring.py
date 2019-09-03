1954483c9ca8328b543dba48617b5bcb64a3e736
Matevz Vucnik <vucnikm@gmail.com>
2019-09-03 16:24:36 +0000

    upadted check-wiring.py script

--- scripts/check-wiring.py
+++ scripts/check-wiring.py
@@ -1,16 +1,18 @@
 import csv
 import time
 import socket
-import subprocess
+import subprocess 
 import zerorpc
-
+import sys
+import os
+ #add avahi damon restart
 def save_ip_txt(ip):
 	f= open("pmc_ip.csv","w+")
 	f.write(ip)
 
 
-
 def get_service(service_name):
+	cntr = 1
 	while True:
 		avahi = subprocess.check_output(["avahi-browse","-rptk","_remote._tcp"])
 		avahi = csv.reader(avahi.decode().split('\n'), delimiter=';')
@@ -19,12 +21,18 @@ def get_service(service_name):
 				try:
 					socket.inet_aton(row[7])
 					srv = row[7] + ":" + row[8]
-					print("Service " + service_name + " discovered: " + srv)
+					#print("Service " + service_name + " discovered: " + srv)
 					return srv
 				except socket.error:
 					pass
-
-		time.sleep(10)
+		time.sleep(5)
+		cntr += 3
+		if cntr > 1:
+			print("Device not found, please try again",end='')
+			exit()
+		
+		
+			
 #first start avahi-daemon with "service_name"
 service_name = "saam-pmc"
 ip = get_service(service_name)
@@ -32,5 +40,14 @@ ip = get_service(service_name)
 
 c = zerorpc.Client()
 c.connect("tcp://%s" % ip)
-msg = c.test("testing: %s" % service_name)
-print(msg)
+
+if True:
+	try:
+		msg = c.test("response: %s" % service_name)
+		print(msg,end='')
+	except:
+		print("Device not found",end='')
+		
+sys.stdout.flush()
+
+
