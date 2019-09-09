e1f38b08bc8d61263f30b686d01511e6dad4e8f5
Jakob <52485152+Jakob589@users.noreply.github.com>
2019-09-09 14:33:32 +0200

    grammar fix

--- frontend/src/procedures/ProcedureTestPMC.jsx
+++ frontend/src/procedures/ProcedureTestPMC.jsx
@@ -9,8 +9,8 @@ class ProcedureTestPMC extends React.Component {
         return (
             <div>
                 <BaseProcedureTest
-                    testInstructions={"Before installing the PMC check the wiring diagram. While connecting current transformers make that you are clipping them around wire in a way that L faces the load and K faces mains. Make sure that the wire being used is the one from the mains. Current transformers can be installed pre or after main breaker or RCD. Voltage wires must be wired same as you would measure voltage with multimeter "}
-                    testTroubleshooting={"Troubleshooting is in response"}
+                    testInstructions={"Before installing the PMC check the wiring diagram. While connecting current transformers make that you are clipping them around wire in a way that L faces the load and K faces mains. Make sure that the wire being used is the one from the mains. Current transformers can be installed pre or after main breaker or RCD. Voltage wires must be wired same as you would measure voltage with multimeter."}
+                    testTroubleshooting={"Troubleshooting is in response."}
                     testAPI="/test/pmc"
                     {...this.props}
                 />
