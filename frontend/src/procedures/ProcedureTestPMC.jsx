561ce1a65434bebe14ab2a2652c5492382e01246
Jakob <52485152+Jakob589@users.noreply.github.com>
2019-09-09 11:36:23 +0200

    Added description

--- frontend/src/procedures/ProcedureTestPMC.jsx
+++ frontend/src/procedures/ProcedureTestPMC.jsx
@@ -9,8 +9,8 @@ class ProcedureTestPMC extends React.Component {
         return (
             <div>
                 <BaseProcedureTest
-                    testInstructions={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur at diam mauris. Donec lorem magna, fringilla id nisi eget, sagittis pellentesque orci. Donec ullamcorper vel sapien pharetra hendrerit. Fusce vitae felis fringilla, faucibus lectus nec, malesuada urna. Aliquam maximus vestibulum nibh. Curabitur porta leo elit, sodales euismod ex feugiat eu. Ut convallis, massa sit amet auctor aliquet, eros velit mattis justo, fermentum hendrerit magna metus sed magna. Pellentesque eget mi pulvinar, porttitor turpis vitae, gravida eros. Cras urna risus, sollicitudin id magna facilisis, rutrum dapibus felis. Integer varius sapien eros, nec suscipit magna pharetra id. Aliquam in purus congue, elementum enim id, accumsan metus. Suspendisse quis ex sed nisi feugiat feugiat id eu ex. Vestibulum lacinia ligula et lectus porta egestas. Donec facilisis tortor ut tristique sodales."}
-                    testTroubleshooting={"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget."}
+                    testInstructions={"Before installing the PMC check the wiring diagram. While connecting current transformers make that you are clipping them around wire in a way that L faces the load and K faces mains. Make sure that the wire being used is the one from the mains. Current transformers can be installed pre or after main breaker or RCD. Voltage wires must be wired same as you would measure voltage with multimeter "}
+                    testTroubleshooting={"Troubleshooting is in response"}
                     testAPI="/test/pmc"
                     {...this.props}
                 />
