21fc2e4e9e12955b03035973bfa576699bb81895
Jakob <52485152+Jakob589@users.noreply.github.com>
2019-09-09 15:18:14 +0200

    updated for compatibility

--- scripts/check-UWB.py
+++ scripts/check-UWB.py
@@ -4,12 +4,12 @@ import zerorpc
 c = zerorpc.Client()
 c.connect("tcp://127.0.0.1:4242")
 
-if (c.checkActiveDevices() == OK and c.checkUWBLayout == OK):
+if (c.checkActiveDevices() == "OK" and c.checkUWBLayout == "OK"):
    print("OK")
 
-elif c.checkActiveDevices() != OK:
+elif c.checkActiveDevices() != "OK":
    print("ERROR: not enough devices connected")
  
-elif c.checkUWBLayout != OK:
+elif c.checkUWBLayout() != "OK":
    print("ERROR: Bad placement, please place devices in a triangular shape")
 
