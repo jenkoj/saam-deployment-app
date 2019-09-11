3c25e477b9bbf95ca73a6d13c126ea333a2c4efb
Jakob <52485152+Jakob589@users.noreply.github.com>
2019-09-11 14:34:56 +0200

    Update ProcedureTestUWB.jsx

--- frontend/src/procedures/ProcedureTestUWB.jsx
+++ frontend/src/procedures/ProcedureTestUWB.jsx
@@ -10,7 +10,7 @@ class ProcedureTestUWB extends React.Component {
             <div>
                 <BaseProcedureTest
                     testInstructions={"Two UWB devices in a socket-on-socket enclosures should be placed in different parts of an appartment and not obstructed by any big obstructions (e.g. placed behind sofa etc.). All three UWB devices should be deployed in a shape as close to equilateral triangle shape as possible. If they are placed in a straight line (colinear) the system can’t work and test will return the layout error."}
-                    testTroubleshooting={"In case you system reports an error even though your positioning is correct, unplug and plug back edge gateway and UWB sockets, then try again"}
+                    testTroubleshooting={"In case you system reports an error even though your positioning is correct, unplug and plug back edge gateway and UWB sockets, then try again."}
                     testAPI="/test/uwb"
                     {...this.props}
                 />
